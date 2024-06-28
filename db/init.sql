9c0f033cdef2939668efd309a286206fed2da079
Mark Allison <markallison2001@hotmail.com>
2024-06-28 23:24:34 +0100

    new database table

--- db/init.sql
+++ db/init.sql
@@ -4,3 +4,12 @@ CREATE TABLE movies (
     poster VARCHAR(255)
 );
 
+CREATE TABLE movies_to_watch (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    poster_path VARCHAR(255) NOT NULL,
+    time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
