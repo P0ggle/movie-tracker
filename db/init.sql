f0beb58142b38e530a461862122f354b227ac5f3
Mark Allison <markallison2001@hotmail.com>
2024-07-02 23:10:43 +0100

    added row to movies_to_watch table

--- db/init.sql
+++ db/init.sql
@@ -8,7 +8,8 @@ CREATE TABLE movies_to_watch (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     poster_path VARCHAR(255) NOT NULL,
-    time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    time_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    watched BOOLEAN DEFAULT FALSE
 );
 
 
