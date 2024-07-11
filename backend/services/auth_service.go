54d50bf26d4ed1e433033ebe5b9c335499167ff5
Mark Allison <markallison2001@hotmail.com>
2024-07-11 23:15:41 +0100

    move jwt key to .env

--- backend/services/auth_service.go
+++ backend/services/auth_service.go
@@ -1,15 +1,32 @@
 package services
 
 import (
+	"log"
 	"movie-app/models"
 	"movie-app/repositories"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
-var JwtKey = []byte("SqYlIYQNBrUFu29bW8K4cCWarKiQetVKw2cUwx8pMSg=")
+var JwtKey []byte
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	jwtKey := os.Getenv("JWT_KEY")
+	if jwtKey == "" {
+		log.Fatalf("JWT_KEY not found in .env file")
+	}
+
+	JwtKey = []byte(jwtKey)
+}
 
 type AuthService struct {
 	UserRepo *repositories.UserRepository
