0eb62e71b306acda3275dc88ffce348d34ba95fc
Mark Allison <markallison2001@hotmail.com>
2024-07-12 22:37:08 +0100

    adding in read only and read write users to init script

--- db/init.sql
+++ db/init.sql
@@ -14,3 +14,24 @@ CREATE TABLE movies_to_watch (
     watched BOOLEAN DEFAULT FALSE,
     user_id INT NOT NULL REFERENCES users(id)
 );
+
+-- read only user creation
+CREATE USER movie_db_ro WITH PASSWORD 'DevPass123';
+
+GRANT CONNECT ON DATABASE my_movie_db TO movie_db_ro;
+GRANT USAGE ON SCHEMA public TO movie_db_ro;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO movie_db_ro;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO movie_db_ro;
+
+-- read write user creation
+CREATE USER movie_db_rw WITH PASSWORD 'DevPass123';
+
+GRANT CONNECT ON DATABASE my_movie_db TO movie_db_rw;
+GRANT USAGE ON SCHEMA public TO movie_db_rw;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO movie_db_rw;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO movie_db_rw;
+
